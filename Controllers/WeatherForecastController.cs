e4308841f41b96d8ae02f2d754d261877eb9763f
ckomalram <glaw14@gmail.com>
2023-03-24 14:25:34 -0500

    ++ Atributos para verbos http

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,22 +11,46 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
+
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        if (ListWeatherForecast == null || !ListWeatherForecast.Any())
+        {
+            ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+            //.ToArray();
+
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return ListWeatherForecast;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherforecast)
+    {
+        ListWeatherForecast.Add(weatherforecast);
+
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index){
+
+        ListWeatherForecast.RemoveAt(index);
+        return Ok();
     }
 }
---
+++ comandos.md
@@ -0,0 +1,4 @@
+# Links
+
+https://www.toptal.com/developers/gitignore
+https://www.postgresql.org/
